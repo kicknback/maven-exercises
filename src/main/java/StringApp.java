7416b492cf8398efcc88b65dea17ee48f8e7ddfe
kicknback <joshua.borreli@gmail.com>
2021-07-20 18:04:01 -0500

    Finished brief Maven curriculum exercise

--- src/main/java/StringApp.java
+++ src/main/java/StringApp.java
@@ -1,8 +1,19 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class StringApp {
 
     public static void main(String[] args) {
 
+        StringUtils st = new StringUtils();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter some input: ");
+        String userInput = sc.next();
 
+        System.out.println(st.isNumeric(userInput));
+        System.out.println(st.swapCase(userInput));
+        System.out.println(st.reverse(userInput));
 
     }
 
