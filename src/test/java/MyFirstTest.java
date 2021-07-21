9ab44a381ca0c9c1cd7a60001aab3afe3c7a8534
kicknback <joshua.borreli@gmail.com>
2021-07-21 10:43:48 -0500

    Added more tests, and created codeupcrypt class

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -18,7 +18,28 @@ public class MyFirstTest {
         List<String> languages = new ArrayList<>();
         List<String> moreLanguages = new ArrayList<>();
 
+        assertNotSame(languages, moreLanguages);
+    }
 
+    @Test
+    public void arraysEqual() {
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    @Test
+    public void verifyStatements() {
+        String language = "PHP";
+//        language.contains("H"); // use assertTrue for this statement
+//        language.contains("J"); // use assertFalse for this statement
+
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
     }
 
 }
