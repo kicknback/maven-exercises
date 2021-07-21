945a1eef484785eacf492c3c44eeadf5369b4947
kicknback <joshua.borreli@gmail.com>
2021-07-21 11:59:03 -0500

    Adding .gitignore file, and testing classes for codeupcrypt

---
+++ src/main/java/CodeupCrypt.java
@@ -0,0 +1,46 @@
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
+
+public class CodeupCrypt {
+
+    public static double version;
+
+    public static String hashPassword(String str) {
+//        return Hashing.sha256().hashString(str, StandardCharsets.UTF_8).toString();
+        String hashString = "";
+        char[] strArray = str.toCharArray();
+        for (int i = 0; i < strArray.length; i++) {
+            switch (strArray[i]) {
+                case 'a':
+                case 'A':
+                    hashString += '4';
+                    break;
+                case 'e':
+                case 'E':
+                    hashString += '3';
+                    break;
+                case 'i':
+                case 'I':
+                    hashString += '1';
+                    break;
+                case 'o':
+                case 'O':
+                    hashString += '0';
+                    break;
+                case 'u':
+                case 'U':
+                    hashString += '9';
+                    break;
+                default:
+                    hashString += strArray[i];
+            }
+        }
+        return hashString;
+    }
+
+    public static boolean checkPassword(String pass, String hash) {
+        return hash.equals(pass);
+    }
+
+}
